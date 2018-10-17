14a93ae76c8c109173114a982c1da63b2538c952
Alejandro Such Berenguer <alejandro.such@gmail.com>
2018-10-17 22:32:28 +0200

    initial commit

---
+++ src/main/java/com/veamospues/farmatic2prestashop/Farmatic2prestashopApplication.java
@@ -0,0 +1,12 @@
+package com.veamospues.farmatic2prestashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Farmatic2prestashopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Farmatic2prestashopApplication.class, args);
+    }
+}
