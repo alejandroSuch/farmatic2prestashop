acbc1e95a752fb59780e1b5675ed94405b0b248d
Alejandro Such <alejandro.such@gmail.com>
2020-02-03 23:12:15 +0100

    Cleanup

--- src/main/java/com/veamospues/farmatic2prestashop/route/ProductsFileReader.java
+++ src/main/java/com/veamospues/farmatic2prestashop/route/ProductsFileReader.java
@@ -1,47 +1,58 @@
 package com.veamospues.farmatic2prestashop.route;
 
+import static java.lang.String.format;
+
 import com.veamospues.farmatic2prestashop.config.PrestashopConfiguration;
 import com.veamospues.farmatic2prestashop.domain.StockAvailableRepository;
-import lombok.AllArgsConstructor;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.csv.CsvDataFormat;
 import org.springframework.stereotype.Component;
 
-import static java.lang.String.format;
-
 @Component
 public class ProductsFileReader extends RouteBuilder {
-    private static final String ROUTE_ID = "ProductsFileReader";
-    private static final String PRODUCTS_CSV = "products.csv";
-    private static final String TEN_SECONDS = "10000";
 
-    private StockAvailableRepository stockAvailableRepository;
-    private PrestashopConfiguration prestashopConfiguration;
-    private CsvDataFormat csv;
+  private static final String ROUTE_ID = "ProductsFileReader";
+  private static final String PRODUCTS_CSV = "products.csv";
+  private static final String TEN_SECONDS = "10000";
 
-    public ProductsFileReader(CamelContext context, StockAvailableRepository stockAvailableRepository, PrestashopConfiguration prestashopConfiguration, CsvDataFormat csv) {
-        super(context);
-        this.stockAvailableRepository = stockAvailableRepository;
-        this.prestashopConfiguration = prestashopConfiguration;
-        this.csv = csv;
-    }
+  private StockAvailableRepository stockAvailableRepository;
+  private PrestashopConfiguration prestashopConfiguration;
+  private CsvDataFormat csv;
 
-    @Override
-    public void configure() throws Exception {
-        from(productsFile())
-                .routeId(ROUTE_ID)
-                .unmarshal(csv)
-                .process(stockAvailableRepository)
-                .to(format("controlbus:route?routeId=%s&action=stop", StockVariationsReader.ROUTE_ID))
-                .to(format("controlbus:route?routeId=%s&action=start", StockVariationsReader.ROUTE_ID))
-                .to(format("controlbus:route?routeId=%s&action=stop", FullStockProcessor.ROUTE_ID))
-                .to(format("controlbus:route?routeId=%s&action=start", FullStockProcessor.ROUTE_ID))
-                .end()
-        ;
-    }
+  public ProductsFileReader(
+    CamelContext context,
+    StockAvailableRepository stockAvailableRepository,
+    PrestashopConfiguration prestashopConfiguration,
+    CsvDataFormat csv
+  ) {
+    super(context);
+    this.stockAvailableRepository = stockAvailableRepository;
+    this.prestashopConfiguration = prestashopConfiguration;
+    this.csv = csv;
+  }
 
-    private String productsFile() {
-        return "file:" + prestashopConfiguration.getCsvLocation() + "?fileName=" + PRODUCTS_CSV + "&noop=true&idempotentKey=${file:name}-${file:modified}&delay=" + TEN_SECONDS;
-    }
+  @Override
+  public void configure() {
+    from(productsFile())
+      .routeId(ROUTE_ID)
+      .unmarshal(csv)
+      .process(stockAvailableRepository)
+      .to(format("controlbus:route?routeId=%s&action=stop", StockVariationsReader.ROUTE_ID))
+      .to(format("controlbus:route?routeId=%s&action=start", StockVariationsReader.ROUTE_ID))
+      .to(format("controlbus:route?routeId=%s&action=stop", FullStockProcessor.ROUTE_ID))
+      .to(format("controlbus:route?routeId=%s&action=start", FullStockProcessor.ROUTE_ID))
+      .end()
+    ;
+  }
+
+  private String productsFile() {
+    return "file:" +
+      prestashopConfiguration.getCsvLocation() +
+      "?fileName=" +
+      PRODUCTS_CSV
+      + "&noop=true&idempotentKey=${file:name}-${file:modified}&delay=" +
+      TEN_SECONDS
+    ;
+  }
 }
